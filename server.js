61e8f8b043a24c07f480411372519690445eb5b0
Bushjdo <phuclearn@gmail.com>
2017-07-24 22:41:48 +0700

    fix function fs

--- server.js
+++ server.js
@@ -4,36 +4,25 @@ var fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
-
 hbs.registerPartials(__dirname +'/views/partials');
 hbs.registerHelper('year',()=>{
   return new Date().getFullYear();
 })
 app.set("view engine","hbs");
 
-
 app.use((req , res , next)=>{
   var now = new Date().toString();
   var log = `${now} : ${req.url} , ${req.method}`;
-  fs.appendFileSync("severLog.log", log +'\n',(err)=>{
+  fs.appendFile("severLog.log", log +'\n',(err)=>{
     if(err){
       console.log("Can't write log server..");
     }
   })
-
   next();
 });
 
-
-// app.use((req , res , next)=>{
-//   res.render('maintaince.hbs');
-// });
-
-// app.use se giup express nhan cac ham middleware ma ta muon thuc hien
 app.use(express.static(__dirname +"/public"));
-
 app.get('/',(req ,res)=>{
-  // to pass the data to template , we must specify the object in second agrument ( object : object)
   res.render('about',{
     pageTitle:"Bushjdo page"
 
@@ -48,12 +37,21 @@ app.get('/json',function(req , res){
   })
 });
 
-app.get('/help',(req , res)=>{
 
-});
-
-
-
-app.listen(3000,()=>{
+app.listen(port,()=>{
   console.log(`Server is starting on....${port}`);
 })
+
+
+
+
+
+
+
+
+
+// app.use((req , res , next)=>{
+//   res.render('maintaince.hbs');
+// });
+
+// app.use se giup express nhan cac ham middleware ma ta muon thuc hien
